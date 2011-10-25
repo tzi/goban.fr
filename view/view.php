197d145aba9e0a9fd8a778d6e2e3d57557819b43
Thomas ZILLIOX <thomas@zilliox.me>
2011-10-25 14:00:11 +0200

    Add uservoice script

--- view/view.php
+++ view/view.php
@@ -29,5 +29,13 @@
 			<?= $main_content ?>
 		</div>
 	</div>
+<script type="text/javascript">
+  var uvOptions = {};
+  (function() {
+    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/7GhyH3TUrgBHaHm1vnOeag.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+  })();
+</script>
 </body>
 </html>
